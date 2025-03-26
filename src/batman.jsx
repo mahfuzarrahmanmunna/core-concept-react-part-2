0263af87ad7b916291ee061b07f5edc4df9ab8ad
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-03-26 22:39:12 +0600

    added batman.jsx file

--- src/batman.jsx
+++ src/batman.jsx
@@ -2,17 +2,27 @@ import { useState } from "react"
 
 export default function BatMan() {
     const [runs, setRuns] = useState(0)
+    const [six, setSix] = useState(0);
+    const [four, setFour] = useState(0);
+    // const sixes = () => {
+    //     let updatedSixes = six + 1;
+    //     setSix(updatedSixes)
+    // }
     const handleSingle = () => {
         let newRuns = runs + 1;
         setRuns(newRuns)
     }
     const handleFour = () => {
         let newRuns = runs + 4;
-        setRuns(newRuns)
+        let newFour = four + 1;
+        setRuns(newRuns);
+        setFour(newFour)
     }
     const handleSix = () => {
         let updatedRuns = runs + 6;
-        setRuns(updatedRuns)
+        let updatedSixes = six + 1;
+        setRuns(updatedRuns);
+        setSix(updatedSixes)
     }
     return (
         <div>
@@ -20,6 +30,8 @@ export default function BatMan() {
             {
                 runs > 50 && <p>Congratulations Your score is 50</p>
             }
+            <p><small>Four : {four}</small></p>
+            <p><small>sixes : {six}</small></p>
             <h1>Score : {runs} </h1>
             <button onClick={handleSingle}>Singles</button>
             <button onClick={handleFour}>four</button>
@@ -27,3 +39,22 @@ export default function BatMan() {
         </div>
     )
 }
+
+
+/**
+ * Api fetch
+ *
+ * Norman way
+ */
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+
+// // Async way
+// const fetchData = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = res.json()
+//     console.log(data)
+// }
+// fetchData()
