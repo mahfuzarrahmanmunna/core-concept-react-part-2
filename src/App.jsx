5a92e9f9f446cd6cf66f10e416f3c1f29dd5d6d5
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-03-26 22:59:36 +0600

    added users.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@
 import './App.css'
 import Counter from './counter';
 import BatMan from './batman';
+import Users from './users';
+import { Suspense } from 'react';
+
+
+const fetchUser = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  // .then(data => console.log(data))
 
 function App() {
   function handleClick() {
@@ -16,6 +23,10 @@ function App() {
       <h1>Vite + React</h1>
       <BatMan></BatMan>
       <Counter></Counter>
+
+      <Suspense fallback={<h3> Loading ....</h3>}>
+        <Users fetchUser={fetchUser}></Users>
+      </Suspense>
     </>
   )
 }
